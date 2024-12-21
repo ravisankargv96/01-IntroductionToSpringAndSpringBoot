fb516ab72515ca63b328c902d24ddd9ad533cefd
ravisankargv96 <ravisankargv96@gmail.com>
2024-12-21 22:42:52 +0530

    SpringBoot Introduction

---
+++ IntroductionToSpringBoot/src/test/java/com/codingshuttle/anuj/week1Introduction/introductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.codingshuttle.anuj.week1Introduction.introductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Anuj is running test");
+	}
+
+}
